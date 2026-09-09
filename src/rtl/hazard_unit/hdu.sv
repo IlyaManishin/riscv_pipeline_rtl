64386a2dbe1f006e4e688b46371b8eb4c14facda
IlyaManishin <manishin06@gmail.com>
2026-09-09 21:18:13 +0700

    make valid turn off hdu signals

--- src/rtl/hazard_unit/hdu.sv
+++ src/rtl/hazard_unit/hdu.sv
@@ -21,9 +21,11 @@ module hazard_detection_unit import hazard_unit_pkg::*;
     assign mem_reg_wr = hu_regs_wr.reg_wr_M;
     assign wb_reg_wr  = hu_regs_wr.reg_wr_W;
 
-    assign is_ex_hazard = ex_reg_wr && rsi_cmp.rd_E_valid && (rsi_cmp.eq1_E || rsi_cmp.eq2_E);
+    assign is_ex_hazard  = ex_reg_wr && rsi_cmp.rd_E_valid && (rsi_cmp.eq1_E || rsi_cmp.eq2_E);
     // assign is_mem_hazard = mem_reg_wr && rsi_cmp.rd_M_valid && (rsi_cmp.eq1_M || rsi_cmp.eq2_M);
+    assign is_mem_hazard = '0;
     // assign is_wb_hazard  = wb_reg_wr  && rsi_cmp.rd_W_valid && (rsi_cmp.eq1_W || rsi_cmp.eq2_W);
+    assign is_wb_hazard  = '0;
 
     always_comb begin
         hdu_controls = '0;
